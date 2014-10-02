e0a8fdba0097518fe5bc836d735f7c469e7755b6
Joe Bruno <j1bruno@bridgew.edu>
2014-10-02 10:57:38 -0400

    Started a basic read of the file. Beginning to format the new header.

--- Modify.py
+++ Modify.py
@@ -24,8 +24,27 @@ def constructNewHeader(xData):
     xName = xData[0][0:13]
     xDate = formatDate(xData[0][15:26])
 
+    # Setting the base at a minimum
+    xAge = 1
+
+    # 4th line has time
+    xTime = formatTime(xData[3])
+
+    # Epoch Value
+    xEpoch = calculateEpoch(xData[2])
+
+    xGender = "Ffv"
+    print(xEpoch)
+
+def calculateEpoch(numMin):
+    return (eval(numMin) * 60) // 15
 
 def formatDate(oldDate):
+    # Add the dashes
     return oldDate.replace(" ", "-")
+
+def formatTime(oldTime):
+    # Add a colon and trailing zero's
+    return oldTime[0:2] + ":" + oldTime[2:4] + ":00"
 
 main()
