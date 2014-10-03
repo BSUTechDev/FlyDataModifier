fa132f17e42ed76839eb66972fffca9e629cb07f
Volodimir Duda <volod2010@gmail.com>
2014-10-02 23:18:58 -0400

    Base python script - use to execute app

--- run.py
+++ run.py
@@ -1,1 +1,15 @@
-__author__ = 'Naz'
+#!/usr/bin/python
+__author__ = 'Joe Bruno, Volodimir Duda'
+
+from Interface import Interface
+from Core import Modify
+import os
+
+print("Running...")
+#dirPath = input("Enter DirectoryPath: ")
+dirPath = os.path.join(os.path.join(os.path.dirname(__file__)), "samples")
+print(dirPath)
+Interface.start(dirPath)
+fileList = Interface.populateList()
+Modify.main(fileList)
+print("Done!")
