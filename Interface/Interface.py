f4c55fe14e3d5c09de1eef7ee0bc521352cef955
Volodimir Duda <volod2010@gmail.com>
2014-10-02 23:18:21 -0400

    Adding a base Interface - plans on being CmdLine

--- Interface/Interface.py
+++ Interface/Interface.py
@@ -1,3 +1,21 @@
+#!/usr/bin/python
 __author__ = 'Volodimir Duda'
+
+"""
+Plans to either use a CommandLine Interface or GUI
+"""
+
+import os
+
+def start(dirPath):
+    if os.path.exists(dirPath):
+        print("Working...")
+    else:
+        print("Try again...")
+        raise OSError("Directory Not Found!")
+
+
+def populateList():
+    return []
 
 
