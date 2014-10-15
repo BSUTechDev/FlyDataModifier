38509cafc3a0553967d64c45c2b589f0454c93e5
Volodimir Duda <vduda@NuoDB.com>
2014-10-14 20:29:45 -0400

    Added Scanning and verify capability

--- Interface/Interface.py
+++ Interface/Interface.py
@@ -7,12 +7,20 @@ Plans to either use a CommandLine Interface or GUI
 
 import os
 
+
+
 def start(dirPath):
     if os.path.exists(dirPath):
         print("Working...")
+        for root, dirs, files in os.walk(dirPath):
+
     else:
         print("Try again...")
         raise OSError("Directory Not Found!")
+
+ def verifyFile(file):
+ 	""" Verify correct file to scan; this should be improved in future """
+ 	return file.rfind('.txt') > 0
 
 
 def populateList():
